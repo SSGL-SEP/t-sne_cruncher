a1533b889439313ef82991f3488a5afbb7a6d9c0
Saska Dönges <tuomionpupu@gmail.com>
2017-05-24 17:20:04 +0300

    more tests and codeclimate settings

--- test/utils/test_normalize.py
+++ test/utils/test_normalize.py
@@ -5,4 +5,9 @@ from utils import normalize
 
 class TestNormalize(TestCase):
     def test_normalize(self):
-        pass
+        arr = np.asarray([
+            np.asarray([1, 2]),
+            np.asarray([3, 4])]).astype(np.float64)
+        arr = normalize(arr, 10, 100)
+        self.assertAlmostEqual(np.amax(arr), 100)
+        self.assertAlmostEqual(np.amin(arr), 10)
