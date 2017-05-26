6cc855b8c264c2b3b635058856f3c9c9cd3a8e37
Saska Dönges <tuomionpupu@gmail.com>
2017-05-26 16:01:36 +0300

    more fix

--- test/subprocesses/test_calculate_tsne.py
+++ test/subprocesses/test_calculate_tsne.py
@@ -6,13 +6,17 @@ import subprocesses
 
 class TestCalculateTsne(TestCase):
 
-    @mock.patch("matplotlib.pyplot")
     @mock.patch("builtins.print", autospec=True)
-    def test_calculate_tsne(self, mock_print, mock_matplotlib_pyplot):
+    def test_calculate_tsne(self, mock_print):
         npf = numpy.asarray([[[1, 2, 3], [1, 2, 3], [1, 2, 3]],
                              [[1, 2, 3], [1, 2, 3], [1, 2, 3]],
                              [[1, 2, 3], [1, 2, 3], [1, 2, 3]]]).astype(numpy.float32)
         numpy.load = mock.MagicMock(return_value=npf)
+        matplotlib.pyplot.savefig = mock.MagicMock()
+        matplotlib.pyplot.figure = mock.MagicMock()
+        matplotlib.pyplot.scatter = mock.MagicMock()
+        matplotlib.pyplot.tight_layout = mock.MagicMock()
+        matplotlib.pyplot.close = mock.MagicMock()
         res = subprocesses.calculate_tsne()
         self.assertTrue(mock_print.called, "mock_print not called")
         self.assertTrue(res.shape[1] == 3, "Invalid output shape: {}".format(res.shape))
