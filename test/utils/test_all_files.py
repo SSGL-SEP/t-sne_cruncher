a06ff26e61547d54f92adbd3f28eddacbbdd822a
Saska Dönges <tuomionpupu@gmail.com>
2017-05-29 13:17:18 +0300

    changed call to depreceated function

--- test/utils/test_all_files.py
+++ test/utils/test_all_files.py
@@ -9,7 +9,7 @@ class TestAllFiles(TestCase):
         pa = "root/"
         mock_os_walk.return_value = iter([(pa, [], ['TP_VerbKick-F#3.wav', 'TP_RoomaKick-C4.wav', "data.csv"])])
         res = list(all_files(pa, [".wav"]))
-        self.assertEquals(len(res), 2, "Unexpected number of elements returned: {}.".format(len(res)))
+        self.assertEqual(len(res), 2, "Unexpected number of elements returned: {}.".format(len(res)))
         self.assertTrue("root/TP_VerbKick-F#3.wav" in res)
         self.assertTrue("root/TP_RoomaKick-C4.wav" in res)
         self.assertFalse("root/data.csv" in res)
