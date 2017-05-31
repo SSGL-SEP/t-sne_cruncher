75f1b7df5668f5a02a5bf1154e285d5210444786
Saska Dönges <tuomionpupu@gmail.com>
2017-05-31 10:01:09 +0300

    more descriptive variable names

--- utils/coloration.py
+++ utils/coloration.py
@@ -1,40 +1,39 @@
 import colorsys
-from functools import reduce
 
 
-def _append_color(e, rgb):
-    e.append("#{0:02x}{1:02x}{2:02x}".format(
+def _append_color(audio_list, rgb):
+    audio_list.append("#{0:02x}{1:02x}{2:02x}".format(
         int(max(0, min(rgb[0], 255))),
         int(max(0, min(rgb[1], 255))),
         int(max(0, min(rgb[2], 255)))))
 
 
-def _set_color(e, color_by, s, c):
-    for d in e[5]:
-        if d["key"] == color_by:
-            rgb = colorsys.hsv_to_rgb(s[d["val"]] / c, 1, 255)
-            _append_color(e, rgb)
+def _set_color(audio_list, color_by, value_set, color_count):
+    for tag_dict in audio_list[5]:
+        if tag_dict["key"] == color_by:
+            rgb = colorsys.hsv_to_rgb(value_set[tag_dict["val"]] / color_count, 1, 255)
+            _append_color(audio_list, rgb)
             return
-    _append_color(e, (255, 255, 255))
+    _append_color(audio_list, (255, 255, 255))
 
 
 def _color_by_tag(data_list, color_by):
-    s = {d["val"]: 0 for e in data_list for d in e[5] if d["key"] == color_by}
-    li = sorted(list(s.keys()))
-    for i in range(len(li)):
-        s[li[i]] = i
-    for e in data_list:
-        _set_color(e, color_by, s, len(li))
+    value_set = {d["val"]: 0 for e in data_list for d in e[5] if d["key"] == color_by}
+    value_list = sorted(list(value_set.keys()))
+    for i in range(len(value_list)):
+        value_set[value_list[i]] = i
+    for audio_list in data_list:
+        _set_color(audio_list, color_by, value_set, len(value_list))
 
 
 def _color_by_manhattan(data_list):
-    m = max([e[1] + e[2] + e[3] for e in data_list])
-    for e in data_list:
-        rgb = colorsys.hsv_to_rgb((e[1] + e[2] + e[3])/m, 1, 255)
-        _append_color(e, rgb)
+    max_value = max([audio_list[1] + audio_list[2] + audio_list[3] for audio_list in data_list])
+    for audio_list in data_list:
+        rgb = colorsys.hsv_to_rgb((audio_list[1] + audio_list[2] + audio_list[3])/max_value, 1, 255)
+        _append_color(audio_list, rgb)
 
 
-def add_color(data_list, color_by):
+def add_color(data_list, color_by=None):
     if color_by:
         _color_by_tag(data_list, color_by)
     else:
